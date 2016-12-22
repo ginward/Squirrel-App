c339e9eb299d4727e34c1f95352a86fb30227886
班章 <localhost>
2016-12-22 17:12:11 -0500

    added the format for wav

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 const INPUT_BUFFER_LENGTH = 4096;
-const OUTPUT_BUFFER_LENGTH = 4000;
+const OUTPUT_BUFFER_LENGTH = 100000;
 const OUTPUT_SAMPLE_RATE = 16000; //Unit: Hz
 const SERVER_URL = "http://localhost:8080";
 // Deal with prefixed APIs
@@ -55,9 +55,51 @@ recognizer.postMessage=function(cmd){
 		  // Uploaded.
 		  console.log("uploaded");
 		};
-		console.log(data);
-		var blob = new Blob ([data], {type:'audio/wav'});
+		var wav = packageWAV(data);
+		console.log(wav);
+		var blob = new Blob ([wav], {type:'audio/wav'});
 		oReq.send(blob);
 	}
 }
 
+//function to package WAV data
+function packageWAV(data){
+	//the buffer is used to create the WAV file
+	var buffer = new ArrayBuffer(44+data.length*2);
+	var view = new DataView(buffer);
+	//write the WAV container 
+	//RIFF chunk descriptor 
+	writeUTFBytes(view, 0, "RIFF");
+	view.setUint32(4, 44+data.length*2, true);
+	writeUTFBytes(view, 8, 'WAVE');
+	//FMT sub-chunk
+	writeUTFBytes(view, 12, 'fmt');
+	view.setUint32(16, 16, true);
+	view.setUint16(20, 1, true);
+	//stereo (2 channels)
+	view.setUint16(22, 2, true);
+	view.setUint32(24, OUTPUT_SAMPLE_RATE, true);
+	view.setUint32(28, OUTPUT_SAMPLE_RATE * 4, true);
+	view.setUint16(32, 4, true);
+	view.setUint16(34, 16, true);
+	//data sub chunk
+	writeUTFBytes(view, 36, 'data');
+	view.setUint32(40, data.length*2, true);
+
+	//write the PCM samples
+	var lng = data.length; 
+	var index = 44; 
+	var volume = 1; 
+	for (var i=0;i<lng;i++){
+		view.setInt16(index, data[i] *  (0x7FFF * volume), true);
+		index+=2;
+	}
+	return view; 
+}
+
+function writeUTFBytes(view, offset, string){
+	var lng = string.length;
+	for (var i=0; i<lng; i++){
+		view.setUint8(offset+i, string.charCodeAt(i));
+	}
+}

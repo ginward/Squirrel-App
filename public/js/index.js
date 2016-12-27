6d6414de1a98c461e9db65e83564ca8b342db255
班章 <localhost>
2016-12-27 12:49:36 -0500

    added the hint to download chrome from app store

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
 const SERVER_URL = "https://www.duedue.xyz:8080";
 const RECORDING_INTERVAL = 15000; //ms
+//check the browser
+detectSafari();
 //initialize the audio
 initAudio();
 
@@ -253,4 +255,17 @@ function printObject(o) {
     out += p + ': ' + o[p] + '\n';
   }
   alert(out);
+}
+
+//since safari is currently not supported, we want to tell the user to download chrome
+function detectSafari(){
+    if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) 
+    {
+       var r = confirm("We currently do not support Safari. Please download Chrome from App Store.");
+        if (r == true) {
+            window.location.replace("https://itunes.apple.com/us/app/chrome/id535886823");
+        } else {
+            window.location.replace("https://itunes.apple.com/us/app/chrome/id535886823");
+        }
+    }
 }

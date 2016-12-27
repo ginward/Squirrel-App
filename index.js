86697991d6531e92e8b9240830fb9d769d9a9edf
班章 <localhost>
2016-12-27 12:28:38 -0500

    added the keystore for https

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
+var https = require('https');
+var http = require('http');
+var privateKey  = fs.readFileSync('/etc/letsencrypt/live/www.duedue.xyz/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/www.duedue.xyz/cert.pem', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
 var path = require('path');
+app.all('*', ensureSecure); // at top of routing calls
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(81, function () {
-  console.log('Example app listening on port 81!')
-})
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443);
+http.createServer(app).listen(80);
+function ensureSecure(req, res, next){
+  if(req.secure){
+    // OK, continue
+    return next();
+  };
+  // handle port numbers if you need non defaults
+  // res.redirect('https://' + req.host + req.url); // express 3.x
+  res.redirect('https://' + req.hostname + req.url); // express 4.x
+};
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "http://localhost:8080";
+const SERVER_URL = "https://www.duedue.xyz:8080";
 const RECORDING_INTERVAL = 15000; //ms
 //initialize the audio
 initAudio();
